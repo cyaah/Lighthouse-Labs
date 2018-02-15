32aa3e1fbf644ae48047981466caeb7cc625bd2b
Calvin Yah <33641533+cyaah@users.noreply.github.com>
2018-02-15 14:03:11 -0500

    Add files via upload

---
+++ CallbackArguments.js
@@ -0,0 +1,18 @@
+// The second argument/parameter is expected to be a function
+function findWaldo(arr, found) {
+  for (var i = 0; i < arr.length; i++) {
+    if (arr[i] === "Waldo") {
+    	
+      found(i);   // execute callback
+    }
+  }
+}
+
+function actionWhenFound(i) {
+  
+  console.log(`Found waldo at: ${i}`);
+  
+}
+//actionWhenFound is the callback,
+//Basially what were doing is the were calling the function as an argument to another function
+findWaldo(["Alice", "Bob", "Waldo", "Winston"], actionWhenFound);
