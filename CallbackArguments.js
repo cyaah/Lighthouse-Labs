afcaed7c574c6717f4783883cbd7cd1110c68d2a
Calvin Yah <33641533+cyaah@users.noreply.github.com>
2018-02-15 14:07:18 -0500

    Updated to find the index

--- CallbackArguments.js
+++ CallbackArguments.js
@@ -1,11 +1,16 @@
 // The second argument/parameter is expected to be a function
 function findWaldo(arr, found) {
-  for (var i = 0; i < arr.length; i++) {
+  // for (var i = 0; i < arr.length; i++) {
     if (arr[i] === "Waldo") {
     	
       found(i);   // execute callback
     }
   }
+arr.forEach(findWaldo);
+
+
+
+
 }
 
 function actionWhenFound(i) {
